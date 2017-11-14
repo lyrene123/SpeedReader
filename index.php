e9eadab8ead6a1fb99cb105690e2fde97ebbbcab
Lyrene Labor <laborlyrene@gmail.com>
2017-11-14 04:54:25 +0000

    testing

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	}
 } else {
 	if(isset($_GET['submit']) && $_GET['submit'] == 'Logout'){
-		echo "HEREEEE";
+		session_start();
 		$_SESSION = array();
     session_destroy();
 	}
